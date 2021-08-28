10debe6fa656c0cbbc4b813546dd98a6e632618e
g00chy <totugekiluna@gmail.com>
2021-08-28 17:55:00 +0000

    add basic

---
+++ src/components/tutrial/Basic.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export interface Data {
+    hoge: string
+}
+
+Data data = ({
+    hoge: 'min'
+})
+
+export default Vue.extend({
+    data
+})
+</script>
