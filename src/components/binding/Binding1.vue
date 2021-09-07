a68e28742906c065d3ee87eeed0cebb1e5b76a7e
g00chy <totugekiluna@gmail.com>
2021-09-07 13:41:31 +0000

    binding

---
+++ src/components/binding/Binding1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="static" :class="classObject">test</div>
+</template>
+<style>
+.text-danger {
+  color: red;
+}
+.active {
+  font-style: italic;
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+export type DataType = {
+  isActive: boolean;
+  hasError: boolean;
+};
+
+export default Vue.extend({
+  name: "Binding",
+  data(): DataType {
+    return {
+      isActive: true,
+      hasError: true,
+    };
+  },
+  computed: {
+    classObject(): DataType {
+      return {
+        active: this.isActive,
+        "text-danger": this.hasError,
+      };
+    },
+  },
+});
+</script>
