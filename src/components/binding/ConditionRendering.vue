88cf0b74c2c79d788985e1ce6783d2e3a432aea9
g00chy <totugekiluna@gmail.com>
2021-09-11 16:41:38 +0000

    condition

--- src/components/binding/ConditionRendering.vue
+++ src/components/binding/ConditionRendering.vue
@@ -1,29 +1,42 @@
 <template>
   <div>
     <div v-if="ok">
-      <h1>Title</h1>
+      <h3>Title</h3>
       <p>Paragraph 1</p>
       <p>Paragraph 2</p>
     </div>
-    <div v-else>test</div>
-    <div v-show="ok">v-show</div>
-    <div v-bind:style="{ color: activeColor, fontSize: fontSize + 'px' }">
-      ほげぇ
+    <div v-if="rand">
+      <p>rand</p>
     </div>
+    <div v-else-if="rand2"><p>elseif</p></div>
+    <div v-else><p>else</p></div>
+    <input-component></input-component>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import InputComponent from "./child/input.vue";
 
 export default Vue.extend({
   name: "Binding",
+  components: {
+    InputComponent,
+  },
   data() {
     return {
       ok: true,
       activeColor: "red",
       fontSize: 20,
     };
+  },
+  computed: {
+    rand: (): boolean => {
+      return Math.random() > 0.5;
+    },
+    rand2: (): boolean => {
+      return Math.random() > 0.5;
+    },
   },
 });
 </script>
---
+++ src/components/binding/child/input.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h4>with no key</h4>
+    <div v-if="loginType">
+      <label>Username</label>
+      <input placeholder="Enter your username" />
+    </div>
+    <div v-else>
+      <label>Email</label>
+      <input placeholder="Enter your email address" />
+    </div>
+    <button v-on:click="loginChange" value="change">button</button>
+    <h4>with key</h4>
+    <div v-if="loginType">
+      <label>Username</label>
+      <input placeholder="Enter your username" key="username-input" />
+    </div>
+    <div v-else>
+      <label>Email</label>
+      <input placeholder="Enter your email address" key="email-input" />
+    </div>
+    <button v-on:click="loginChange" value="change">button</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      loginType: true,
+    };
+  },
+  methods: {
+    loginChange(): void {
+      console.log("change");
+      this.loginType = !this.loginType;
+    },
+  },
+});
+</script>
