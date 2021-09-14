96b4682d7641a3fa0dd0839dd5e8b9c455e815f9
g00chy <totugekiluna@gmail.com>
2021-09-14 17:07:11 +0000

    message まで

--- src/components/events/event1.vue
+++ src/components/events/event1.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
     <h3>events</h3>
+    <h4>in on click</h4>
+    <button v-on:click="counter += 1">count up</button>
+    <p>count {{ counter }}</p>
+    <h4>method</h4>
+    <button v-on:click="countUp">count up</button>
+    <p>count {{ counter2 }}</p>
+    <h4>$event</h4>
+    <button v-on:click="messageAndEvent('wow', $event)">button</button>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+
+
+export default Vue.extend({
+  data: () => {
+    return {
+      counter: 0,
+      counter2: 0,
+      message: "",
+    };
+  },
+  methods: {
+    countUp: function (): void {
+      this.counter2 += 1;
+    },
+    messageAndEvent(message: string, events: Event): void {
+      if(events){
+        events.preventDefault();
+      }
+      this.message = message;
+    },
+  },
+});
 </script>
