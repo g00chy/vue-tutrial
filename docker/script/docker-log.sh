1fa2d349432f77f2f22f8d96c177b63ab04ecf8e
g00chy <totugekiluna@gmail.com>
2021-09-23 07:51:48 +0000

    jq使うのが正しいや。

--- docker/script/docker-log.sh
+++ docker/script/docker-log.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-IF_EXECED_DOCKER=`docker-compose ps`
-if [ -n "$$IF_EXECED_DOCKER" ]; then
-    make docker-restart
-fi
-docker-compose ps -a | tail -n +3 | awk '{print $1}' | xargs docker inspect | fgrep -i LogPath | awk '{print $2}' | sed -e 's/"//g' | sed -e 's/,//g' | sudo xargs rm 
+docker-compose ps -a | tail -n +3 | awk '{print $1}' | xargs docker inspect |  jq '.[].LogPath' | sudo xargs rm 
 IF_EXECED_DOCKER=`docker-compose ps`
 if [ -n "$$IF_EXECED_DOCKER" ]; then
     make docker-restart
